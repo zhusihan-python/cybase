6faafd66cf51a6b9203112e3b863777dbc831f95
zhusihan <1009698013@qq.com>
2023-07-08 11:29:22 +0800

    fix greatestProduct result not right in question8.pyx

--- euler/question8/question8.pyx
+++ euler/question8/question8.pyx
@@ -1,17 +1,17 @@
 from libcpp.vector cimport vector
 from libcpp.algorithm cimport max_element
+from libc.string cimport strlen
+# from libc.stdio cimport printf
 from cython.operator cimport dereference as deref
 
 def max_multiply():
     cdef int i
-    cdef int len_data
     cdef long long prod
     cdef list sub
     with open('ep08.txt', 'r') as f:
         data = ''
         for line in f.readlines():
             data = data + line.strip()
-    len_data = len(data)
     cdef vector[long long] res
 
     for i in range(988):
@@ -22,3 +22,49 @@ def max_multiply():
         res.push_back(prod)
     cdef vector[long long].iterator it = max_element(res.begin(), res.end())
     return deref(it)
+
+
+cdef long long greatestProduct(char * digits):
+    cdef int i, n = strlen(digits)
+    cdef int j
+    cdef long long prod, maxProd = 0
+
+    for i in range(n - 12):
+        # printf("cur digits[i] %d\n", digits[i] - 48)
+        # printf("cur digits[i+1] %d\n", digits[i + 1] - 48)
+        # printf("cur digits[i+2] %d\n", digits[i + 2] - 48)
+        # printf("cur digits[i+3] %d\n", digits[i + 3] - 48)
+        # printf("cur digits[i+4] %d\n", digits[i + 4] - 48)
+        # printf("cur digits[i+5] %d\n", digits[i + 5] - 48)
+        # printf("cur digits[i+6] %d\n", digits[i + 6] - 48)
+        # printf("cur digits[i+7] %d\n", digits[i + 7] - 48)
+        # printf("cur digits[i+8] %d\n", digits[i + 8] - 48)
+        # printf("cur digits[i+9] %d\n", digits[i + 9] - 48)
+        # printf("cur digits[i+10] %d\n", digits[i + 10] - 48)
+        # printf("cur digits[i+11] %d\n", digits[i + 11] - 48)
+        # printf("cur digits[i+12] %d\n", digits[i + 12] - 48)
+        prod = 1
+        for j in range(13):
+            prod *= (digits[i+j]-48)
+
+        # printf("cur maxProd %d\n", maxProd)
+        # printf("cur prod %d\n", prod)
+        if prod > maxProd:
+            maxProd = prod
+
+    return maxProd
+
+
+def max_multiply_v2():
+    cdef str data
+    cdef bytes data_b
+    cdef char * c_string
+    cdef long long res
+    with open('ep08.txt', 'r') as f:
+        data = ''
+        for line in f.readlines():
+            data = data + line.strip()
+    data_b = bytes(data, encoding="utf-8")
+    c_string = data_b
+    res = greatestProduct(c_string)
+    return res
--- euler/question8/test_question8.py
+++ euler/question8/test_question8.py
@@ -1,12 +1,13 @@
 from question8 import max_multiply_py
-from q8 import max_multiply
+from q8 import max_multiply, max_multiply_v2
 import timeit
 
 
 if __name__ == "__main__":
-    # print(max_multiply_py())
-    # print(max_multiply())
     sec = timeit.repeat('max_multiply_py()', setup='from __main__ import max_multiply_py', number=100)
     print(f"max_multiply_py cost {sec} seconds")
     sec = timeit.repeat('max_multiply()', setup='from __main__ import max_multiply', number=100)
     print(f"max_multiply cost {sec} seconds")
+    sec = timeit.repeat('max_multiply_v2()', setup='from __main__ import max_multiply_v2', number=100)
+    print(f"max_multiply_v2 cost {sec} seconds")
+    # max_multiply_v2 25 times faster than max_multiply_py
